87e168488089d58850998a602c529abbaefcbdd2
aditya25bce11333-byte <aditya.25bce11333@vitbhopal.ac.in>
2026-07-13 22:45:52 +0530

    Time: 8 ms (55.13%) | Memory: 22.9 MB (81.03%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,32 +1,35 @@
 class Solution {
-private :
-    long long calculatetotalhours(vector<int>& piles , int k){
-        long long TotalH = 0;
+    //creating private function , so that it doesn't interact with the public function.
+    // Also it aids in calculating total hours needed by monkey to finish the pile if the eating spped is k bananas/hour.
+private:
+    long long calculatetotalhours (vector<int>& piles , int k){
+        long long totalH = 0;
         for (int bananas : piles){
-            TotalH += (bananas + k - 1) / k;
+            totalH += (long long)(bananas + k - 1) / k;    // shortcut method for floor division and rounding-off answers.
         }
-        return TotalH;
-    }
+        return totalH;
+    }    
 public:
+//function inbuilt for calculating max banana in the pile for setting the high and low pointer for our search approach.
     int minEatingSpeed(vector<int>& piles, int h) {
         int maxPile = 0;
-        for (int bananas : piles){
-            maxPile = max (maxPile , bananas);
-            int ans = maxPile;
+        for ( int bananas : piles){
+            maxPile = max(maxPile,bananas);
         }
-        int low = 1  , high = maxPile;
-        while ( low <= high){
+        int low = 1 , high = maxPile;
+        while ( low <= high ){
             int mid = low + (high-low)/2;
-
-            long long TotalH = calculatetotalhours(piles,mid);
-
-            if (TotalH <= h) {
-                int ans = mid;
+            
+            // calculate total hours at a speed of mid.
+            long long totalH = calculatetotalhours (piles , mid);
+            //checking the condition.
+            if (totalH <= h){
                 high = mid-1;
             }else{
-                low = mid + 1;
+                low = mid +1;
             }
         }
+        //at last when high crosses low , our low will point to minimum speed of eating bananas.
         return low;
     }
 };
