a41dd72fd33864850b289b047d7186f8f1d01062
aditya25bce11333-byte <aditya.25bce11333@vitbhopal.ac.in>
2026-07-14 22:25:46 +0530

    Time: 13 ms (24.82%) | Memory: 26 MB (98.52%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,26 +1,29 @@
 class Solution {
 private:
-    int DivisionSum (vector<int>& nums , int div){
+// helper function for finding the sum when every element is divided by trial div.
+    int DivisionSum(vector<int>& nums, int div) {
         int sum = 0;
-        for (int i = 0; i< nums.size(); i++){
-            sum += ceil ((double)nums[i]/div);
+        for (int i = 0; i < nums.size(); i++) {
+            sum += ceil((double)nums[i] / div);
         }
         return sum;
-    }    
+    }
+
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        if (nums.size() > threshold) return -1;
+        if (nums.size() > threshold)
+            return -1;
 
         int low = 1;
-        int high = *max_element(nums.begin(),nums.end());
-        while ( low <= high ){
-            int mid = low + (high - low)/2;
-            if (DivisionSum(nums , mid) <= threshold){
-                high = mid -1;
+        int high = *max_element(nums.begin(), nums.end());
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (DivisionSum(nums, mid) <= threshold) {
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
-        } 
-        return low ;
+        }
+        return low;
     }
 };
