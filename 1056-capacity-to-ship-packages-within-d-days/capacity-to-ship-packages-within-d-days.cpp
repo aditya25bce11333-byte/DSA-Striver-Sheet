c168c11b3d75bddeb3b30cf123f22cb5ca85faa8
aditya25bce11333-byte <aditya.25bce11333@vitbhopal.ac.in>
2026-07-17 11:28:56 +0530

    Time: 2 ms (99.47%) | Memory: 35.2 MB (19.61%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+// private function for calculating no. of days required if the trial capacity is there.
+    int requiredNoofDays(vector<int>& weights,int capacity){
+        int days = 1;
+        int currentload = 0;
+        for (int w : weights){
+            if ( currentload + w > capacity){
+                days++;
+                currentload = w;
+            } else {
+                currentload += w;
+             }
+        }
+        return days;
+    }    
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left = *max_element(weights.begin(),weights.end());
+        // accumulate gives sum of all elements , just add a '0' at the end.
+        int right = accumulate(weights.begin(),weights.end(),0);
+        while ( left < right){
+            int mid = left + ( right - left) / 2;
+
+            int daysrequired =  requiredNoofDays(weights,mid);
+            if (daysrequired <= days){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
