f8d42a321cfc705a918af595e3be5a7e3835e7ef
aditya25bce11333-byte <aditya.25bce11333@vitbhopal.ac.in>
2026-07-26 20:31:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (51.35%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int digitSum = 0 , temp = x;
+
+        while (temp > 0){
+            digitSum += temp % 10;
+            temp /= 10;
+        }
+        if ( x % digitSum == 0) return digitSum;
+        return -1;  
+    }
+};
